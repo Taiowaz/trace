958717384717d9360040490ecb678a2df8c237ac
HDB00 <1615346889@qq.com>
2023-03-08 19:32:00 +0800

    2023/3/8 19:30 修改信息列表显示问题

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,8 +4,6 @@ $.ajax({
     type: "GET",
     dataType: "json",
     success: function (json) {
-        var list = "<div class=\"col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5\">";
-        list += "<figure class=\"effect-ming tm-video-item\">";
         $.each(json, function (i, info) {
             var img = info.imgList;
             var a;
@@ -15,16 +13,18 @@ $.ajax({
                     break;
                 }
             }
-            list += "<img src=\"" + a.imgPath + "\" alt=\"Image\" style='width:445px;height:250px;overflow: hidden;' class=\"img-fluid\">";
+            var list = "<div class=\"col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5\">";
+            list += "<figure class=\"effect-ming tm-video-item\">";
+            list += "<img src=\"" + a.imgPath + "\" alt=\"Image\" class=\"img-fluid\">";
             list += "<figcaption class=\"d-flex align-items-center justify-content-center\">";
             list += "<h2>寻迹京城</h2>";
-            list += "<a href=\"video-detail.html\">View more</a>";
+            list += "<a href=\"infoDetail.html?id="+info.infoId+"\">View more</a>";
             list += "</figcaption></figure>";
             list += "<div class=\"d-flex justify-content-between tm-text-gray\">";
-            list += "<span>" + info.infoTitle + "</span>";
+            list += "<span style='text-align: center'>" + info.infoTitle + "</span>";
             list += "</div></div>";
-            // list += "<div id='" + info.infoId + "'class='info'><img style='width:400px;height:200px' src='.." + a.imgPath + "' alt='图片'></div>" + "<div style='padding:0 10px 10px 10px;background-color:#ABB9C5;width:400px;'>" + "<span>" + info.infoTitle + "</span></div></div>";
+            $("#info-list").append(list);
         })
-        $("#info-list").append(list);
+
     }
 });
